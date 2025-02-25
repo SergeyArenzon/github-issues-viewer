a2d028de380a93d1a938c5b1a25d0b085c24fd61
SergeyArenzon <arenzonsergey@gmail.com>
2025-02-25 21:27:01 +0200

    fix card click propogation

--- src/components/issue.tsx
+++ src/components/issue.tsx
@@ -14,8 +14,8 @@ import {
 const Issue = ({issue, onClose}) => {
     console.log(issue, issue.title);
   return (
-    <div className="w-full h-full bg-white fixed left-0 top-0 flex justify-center items-center px-10">
-        <Card  className="px-3">
+    <div className="w-full h-full bg-white fixed left-0 top-0 flex justify-center items-center px-10" onClick={onClose}>
+        <Card onClick={(e)=>e.stopPropagation()} className="px-3">
             <CircleX onClick={onClose} className="cursor-pointer"/>
             <CardHeader>
                 <CardTitle>{issue.title} #{issue.number} <Badge variant={issue.state}>{issue.state}</Badge></CardTitle>
